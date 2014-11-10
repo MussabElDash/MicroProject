a2796f180a881fad44726665f38c9adad5d9e99d
Omar Ahmed <oa12gb@50-0-21-17.dsl.static.fusionbroadband.com>
2014-11-10 15:15:28 +0200

    Implementing the Register class

--- src/memory/Register.java
+++ src/memory/Register.java
@@ -1,11 +1,33 @@
 package memory;
 
 public class Register {
-	int value;
-	String title;
-	boolean isZeroRegister = false;
+	private int value = 0;
+	private String title = "NOT SET";
+	private boolean isZeroRegister = false;
 	
-	public Register(){
+	public Register(boolean isZeroRegister, String title){
+		this.isZeroRegister = isZeroRegister;
+		this.title = title;
+		value = 0;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		if(isZeroRegister){
+			throw new UnsupportedOperationException("You can't set the zero register!");
+		}
 		
+		this.value = value;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 }
