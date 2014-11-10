75b08dd5ca7aa77d9be277c9dcff2746c6b8de1c
Rami Khalil <thesavagekoder@gmail.com>
2014-11-10 14:33:25 +0200

    Assembler p1

---
+++ src/instructions/isa/Jalr.java
@@ -0,0 +1,24 @@
+package instructions.isa;
+
+import instructions.Instruction;
+
+public class Jalr extends Instruction {
+
+	protected Jalr(String[] params) {
+		super(params);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String machinecode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/instructions/isa/Nand.java
@@ -0,0 +1,24 @@
+package instructions.isa;
+
+import instructions.Instruction;
+
+public class Nand extends Instruction {
+
+	protected Nand(String[] params) {
+		super(params);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String machinecode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
