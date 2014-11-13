aca961d01f24f89906ad56ba20558a0436685ce5
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-11-13 09:47:11 +0200

    Added getMachineCode for Beq

--- src/instructions/isa/Beq.java
+++ src/instructions/isa/Beq.java
@@ -1,23 +1,24 @@
 package instructions.isa;
 
 import instructions.Instruction;
+import utilities.Utilities;
 
 public class Beq extends Instruction {
 
 	protected Beq(String[] params) {
-		super(params, new String[]{"regA", "regB", "imm"}, "0110");
+		super(params, new String[] { "regA", "regB", "imm" }, "0110");
 	}
 
 	@Override
 	public void execute() {
-		if(mem.getRegister(regA) == mem.getRegister(regB))
+		if (mem.getRegister(regA) == mem.getRegister(regB))
 			mem.setRegister("PC", mem.getRegister("PC") + 1 + immValue);
 	}
 
 	@Override
-	public String machinecode() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getMachineCode() {
+		return getOpcode() + Utilities.getBinaryNumber(getRegANum())
+				+ Utilities.getBinaryNumber(getRegBNum())
+				+ Utilities.getBinaryNumber(getImmValue());
 	}
-
 }
