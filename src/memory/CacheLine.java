ec9839aee5dc661f73c5fd45c7fc6724c71c4c20
Omar Ahmed <oa12gb@Omars-MacBook-Pro-6.local>
2014-11-24 22:09:53 +0200

    Added reading data and replacing line

--- src/memory/CacheLine.java
+++ src/memory/CacheLine.java
@@ -4,8 +4,38 @@ import java.util.ArrayList;
 
 public class CacheLine {
 	private ArrayList<String> line = new ArrayList<String>();
+	private int size;
+	private boolean dirty = false;
+	private String tag = null;
 	
-	public CacheLine(){
-		
+	public CacheLine(int size){
+		this.size = size;
+		for(int q = 0; q < size; q++){
+			line.add(null);
+		}
+	}
+	
+	public void setLine(ArrayList<String> line){
+		this.line = line;
+	}
+	
+	public String getBlock(int offset){
+		return line.get(offset);
+	}
+	
+	public boolean isDirty(){
+		return dirty;
+	}
+	
+	public void setDirty(boolean dirty){
+		this.dirty = dirty;
+	}
+	
+	public String getTag(){
+		return tag;
+	}
+	
+	public void setTag(String tag){
+		this.tag = tag;
 	}
 }
--- src/memory/CacheLineSet.java
+++ src/memory/CacheLineSet.java
@@ -1,5 +1,62 @@
 package memory;
 
-public class CacheLineSet {
+import java.util.ArrayList;
 
+public class CacheLineSet {
+	private ArrayList<CacheLine> lineSet = new ArrayList<CacheLine>();
+	private int associativity;
+	
+	public CacheLineSet(int associativity, int lineSize){
+		this.associativity = associativity;
+		for(int q = 0; q < associativity; q++){
+			lineSet.add(new CacheLine(lineSize));
+		}
+	}
+	
+	public int searchTags(int tag){
+		return searchTags(tag + "");
+	}
+	
+	// Returns -1 if not found
+	public int searchTags(String tag){
+		for(int q = 0; q < associativity; q++){
+			if(lineSet.get(q).getTag().equals(tag)){
+				return q;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public CacheLine getCacheLine(int index){
+		return lineSet.get(index);
+	}
+	
+	public void insert(int index, String tag, ArrayList<String> value){
+		String currentTag = lineSet.get(index).getTag();
+		currentTag = tag;
+		
+		CacheLine currentLine = lineSet.get(index);
+		currentLine.setLine(value);
+		
+	}
+	
+	public CacheLine getLineIndexToReplace(){
+		int notDirtyIndex = -1;
+		for(int q = 0; q < associativity; q++){
+			CacheLine line = lineSet.get(q);
+			if(line.getTag() == null){
+				return line;
+			}
+			if(notDirtyIndex == -1 && !line.isDirty()){
+				notDirtyIndex = q;
+			}
+		}
+		
+		if(notDirtyIndex != -1){
+			return lineSet.get(notDirtyIndex);
+		}
+		
+		return lineSet.get(0);
+	}
 }
