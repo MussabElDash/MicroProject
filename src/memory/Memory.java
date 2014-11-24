21d309c39057e9fe3508296d2a5372cd2763646a
Omar Ahmed <oa12gb@Omars-MacBook-Pro-6.local>
2014-11-24 16:48:01 +0200

    Trashing out useless zip file

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -5,52 +5,52 @@ import utilities.Utilities;
 public class Memory {
 	public static int NOT_FOUND_VALUE = -1111111111;
 	private static Memory rootMemory = new Memory();
-	
+
 	private Cache cache = null;
 	private Register[] registers = new Register[8];
 	private int instructionsStartAddress;
-	
-	public static Memory getInstance(){
+
+	public static Memory getInstance() {
 		return rootMemory;
 	}
-	
-	public int getRegisterId(String registerTitle){
-		for(int q = 0; q < 8; q++){
-			if(registers[q].getTitle().equals(registerTitle)){
+
+	public int getRegisterId(String registerTitle) {
+		for (int q = 0; q < 8; q++) {
+			if (registers[q].getTitle().equals(registerTitle)) {
 				return q;
 			}
 		}
-		
+
 		return NOT_FOUND_VALUE;
 	}
-	
-	public void setRegisterValue(String registerTitle, int value){
+
+	public void setRegisterValue(String registerTitle, int value) {
 		int registerId = getRegisterId(registerTitle);
-		
-		if(registerId == NOT_FOUND_VALUE){
+
+		if (registerId == NOT_FOUND_VALUE) {
 			Utilities.raiseError("Register " + registerTitle + " not found!");
 			return;
 		}
-		
+
 		registers[registerId].setValue((short) value);
 	}
-	
-	public int getRegisterValue(String registerTitle){
+
+	public int getRegisterValue(String registerTitle) {
 		int registerId = getRegisterId(registerTitle);
-		
-		if(registerId == NOT_FOUND_VALUE){
+
+		if (registerId == NOT_FOUND_VALUE) {
 			Utilities.raiseError("Register " + registerTitle + " not found!");
 			return NOT_FOUND_VALUE;
 		}
-		
+
 		return registers[registerId].getValue();
 	}
-	
-	public void setMemoryValue(int memoryAddress, int value){
-		
+
+	public void setMemoryValue(int memoryAddress, int value) {
+
 	}
-	
-	public int getMemoryValue(int memoryAddress){
+
+	public int getMemoryValue(int memoryAddress) {
 		return 0;
 	}
 }
