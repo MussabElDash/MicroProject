5fc3bb0a7f984f6c059b132e2f3b34b9b5bcd0b8
Mussab ElDash <mussab14@gmail.com>
2014-11-24 22:03:30 +0200

    add AMAT to final Window

--- src/gui/CacheHitWindow.java
+++ src/gui/CacheHitWindow.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
@@ -18,19 +19,32 @@ public class CacheHitWindow extends JFrame {
 	private static final long serialVersionUID = 8295209798709734582L;
 	private JPanel contentPane;
 
+	public static void main(String[] args) {
+		new CacheHitWindow(null, 5.3);
+	}
+
 	/**
 	 * Create the frame.
+	 * 
+	 * @param amat
 	 */
-	public CacheHitWindow(ArrayList<Pair<Integer, Integer>> cachesHits) {
+	public CacheHitWindow(ArrayList<Pair<Integer, Integer>> cachesHits,
+			double amat) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-		int x = cachesHits.size();
-		for (int i = 1; i <= x; i++) {
-			Pair<Integer, Integer> pair = cachesHits.get(i - 1);
-			contentPane.add(new CacheHitPanel(i, pair.first, pair.second));
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("AMAT:\t"));
+		panel.add(new JLabel(amat + " seconds"));
+		contentPane.add(panel);
+		if (cachesHits != null) {
+			int x = cachesHits.size();
+			for (int i = 1; i <= x; i++) {
+				Pair<Integer, Integer> pair = cachesHits.get(i - 1);
+				contentPane.add(new CacheHitPanel(i, pair.first, pair.second));
+			}
 		}
 
 		JScrollPane scrollPane = new JScrollPane(contentPane);
