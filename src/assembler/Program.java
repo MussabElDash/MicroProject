e801c25e5669071e3b4c51e0c3a51006e2b3db97
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-11-24 22:43:23 +0200

    Sent statistics

--- src/assembler/Program.java
+++ src/assembler/Program.java
@@ -13,6 +13,7 @@ import memory.Memory;
 public class Program {
 	Memory memory;
 	int startAddress;
+	int endAddress;
 	int numOfInstructions;
 
 	public Program(String code, int startAddress, int MemAccessTime,
@@ -28,28 +29,28 @@ public class Program {
 		memory.initialize(caches, MemAccessTime, instructions, startAddress,
 				editedAddress);
 		this.startAddress = startAddress;
-		this.numOfInstructions = instructions.length;
+		this.endAddress = startAddress + (instructions.length - 1);
+		this.numOfInstructions = 0;
 		this.execute();
 		this.afterExec();
 	}
 
 	public void execute() {
+		int val = 0;
 		memory.setRegisterValue("PC", startAddress);
-		for (int i = 0; i < numOfInstructions; i++) {
-			int val = memory.getRegisterValue("PC");
+		do {
+			val = memory.getRegisterValue("PC");
 			Instruction current = memory.getInstruction(val);
 			memory.setRegisterValue("PC", val + 1);
 			current.execute();
-		}
+			numOfInstructions ++;
+		} while(val != endAddress);
 	}
 
 	public void afterExec() {
 		RegistersTable.updateRegisters();
-		double amat = 0;
-		// arraylist(i).first Hit
-		// arraylist(i).second requests
-		ArrayList<Pair<Integer, Integer>> hitRatios;
-		hitRatios = new ArrayList<Pair<Integer, Integer>>();
+		double amat = memory.getAMAT();
+		ArrayList<Pair<Integer, Integer>> hitRatios = memory.getCacheStats();
 		new CacheHitWindow(hitRatios, amat);
 	}
 }
