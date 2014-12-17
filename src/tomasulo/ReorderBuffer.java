af99aea579240c440aeeae4c116494a6260ac8b5
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-12-17 12:37:00 +0200

    Updates

--- src/tomasulo/ReorderBuffer.java
+++ src/tomasulo/ReorderBuffer.java
@@ -1,44 +1,50 @@
 package tomasulo;
 
 import instructions.Instruction;
+import instructions.isa.Jalr;
 
 import java.util.ArrayList;
 
+import memory.Memory;
 import memory.Register;
 
 public class ReorderBuffer {
 	private static ArrayList<ReorderBufferElement> table;
 	private static int size;
 	private static int cnt = 0;
-	
+
 	public static void init(int sz) {
 		size = sz;
 		table = new ArrayList<ReorderBufferElement>(size);
 	}
-	
+
 	public static void insert(String dest) {
-		table.add(new ReorderBufferElement(cnt%size, dest, "", false));
-		cnt ++;
+		table.add(new ReorderBufferElement(cnt % size, dest, "", false));
+		Memory.getInstance().getRegister(dest).setROBNum(cnt % size);
+		cnt++;
 	}
-	
+
 	public static int getFirst() {
 		return table.get(0).getIdx();
 	}
-	
+
 	public static void removeFirst() {
 		if (table.size() > 0) {
 			table.remove(0);
 		}
 	}
-	
+
 	public static boolean isFull() {
 		return (table.size() == size);
 	}
-	
+
 	public static void issue(Instruction instruction) {
-		String dest = null;
-		if (instruction.getType() != RSType.ST) {
+		String dest = "";
+		if (instruction.getType() != RSType.ST
+				&& (instruction.getType() != RSType.JMP || (instruction
+						.getType() == RSType.JMP && instruction instanceof Jalr))) {
 			dest = instruction.getRegA();
 		}
+		insert(dest);
 	}
 }
